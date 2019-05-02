dc5077d8964443b651e7dc6665cc00ddb990b4c5
cap-ljf <1579461369@qq.com>
2019-05-02 16:16:41 +0800

    schedule spring定时任务

---
+++ src/main/java/org/capljf/bigspring/schedule/ScheduledTasks.java
@@ -0,0 +1,25 @@
+package org.capljf.bigspring.schedule;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * spring 定时任务
+ * @author jifang.liu created on 2019/5/2 16:12
+ */
+@Component
+@Slf4j
+public class ScheduledTasks {
+
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+
+    @Scheduled(fixedRate = 5000)
+    public void reportCurrentTime() {
+        log.info("现在时间：" + dateFormat.format(new Date()));
+    }
+
+}
